a754c97445ec9b6c5710e28bae29b40c9c68a605
Liam-gosnell <liamgoz99@gmail.com>
2020-07-14 11:49:10 +0000

    Added products views and templates

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+
+def all_services(request):
+    """ A view to show all services """
+
+    items = Item.objects.all()
+
+    context = {
+        'items': items,
+    }
+
+    return render(request, 'orders/services.html', context)
