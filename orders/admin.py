14f33b22930d4a50c31e1285eba11f20afdb8532
Liam-gosnell <liamgoz99@gmail.com>
2020-07-14 11:04:56 +0000

    Customized admin

--- orders/admin.py
+++ orders/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from .models import Item, ItemTag 
 
 
-admin.site.register(Item, )
-admin.site.register(ItemTag, )
+class ItemTagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'colour'
+    )
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'tag',
+        'price',
+        'clicks'
+    )
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(ItemTag, ItemTagAdmin)
