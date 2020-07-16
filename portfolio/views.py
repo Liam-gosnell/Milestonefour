94b7ce2c8ad5bcd377babda720a2855370d89383
Liam-gosnell <liamgoz99@gmail.com>
2020-07-16 13:52:47 +0000

    Added portfolio basic template

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Item
+from django.shortcuts import render
+from .models import Portfolio
 
 # Create your views here.
+
+def all_portfolio(request):
+    """ A view to return the index page """
+
+    portfolio = Portfolio.objects.all()
+
+    context = {
+        'portfolio': portfolio,
+    }
+
+    return render(request, 'portfolio/portfolio.html', context)
