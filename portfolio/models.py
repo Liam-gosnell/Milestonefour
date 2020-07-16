05d02b21abf9282bda3e1fb8a5d260418a969b63
Liam-gosnell <liamgoz99@gmail.com>
2020-07-16 10:51:15 +0000

    Added styles to mobile, Added 2 portfolio fixtures, models

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,3 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+class Portfolio(models.Model):
+    name = models.CharField(max_length=254)
+    title = models.CharField(max_length=254, null=True, blank=True)
+    slug = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    big_image = models.ImageField(null=True, blank=True)
+    image_1 = models.ImageField(null=True, blank=True)
+    image_2 = models.ImageField(null=True, blank=True)
+    bullet_1 = models.TextField()
+    bullet_2 = models.TextField()
+    bullet_3 = models.TextField()
+    bullet_4 = models.TextField()
+    approach = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
