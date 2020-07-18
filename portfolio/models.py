0d47030144fd6c742075c81d350d26b8ad241ce1
Liam-gosnell <liamgoz99@gmail.com>
2020-07-18 09:35:39 +0000

    Added All images to portfolio section for each portfolio, Added buttons on each page, Added Css styling

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Portfolio(models.Model):
 
     class Meta:
-        verbose_name_plural = 'Portfolio'
+        verbose_name_plural = 'Portfolios'
 
         
     name = models.CharField(max_length=254)
