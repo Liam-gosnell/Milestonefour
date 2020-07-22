c20b0ee9ab96395d138b9b0e68b5afaa814cac47
Liam-gosnell <liamgoz99@gmail.com>
2020-07-22 11:30:31 +0000

    Added subtotal logic

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -22,3 +22,43 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the specified service to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if item_id:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            del bag[item_id]
+
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop[item_id]
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Remove the service from the shopping bag """
+    try:
+        bag = request.session.get('bag', {})
+
+        if item_id:
+                del bag[item_id]
+
+        else:
+                bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
