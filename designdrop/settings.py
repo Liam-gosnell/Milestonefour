217d5a684b110b63d608015f9b2f82363e84e130
Liam-gosnell <liamgoz99@gmail.com>
2020-07-24 16:20:42 +0000

    debug fixed

--- designdrop/settings.py
+++ designdrop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = OS.ENVIRON.GET('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['designdrop.herokuapp.com', 'localhost']
 
