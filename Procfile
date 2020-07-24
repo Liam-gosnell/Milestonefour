014484c6229dc92e39bb8eddc7f4800bf10b009b
Liam-gosnell <liamgoz99@gmail.com>
2020-07-24 15:04:15 +0000

    deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn designdrop.wsgi:application
--- dropdesign/settings.py
+++ dropdesign/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '**!ytja3++hs@+*l^-v-3coxg8=+j=r2)-s9f51od(svenwuiz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['designdrop.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'dropdesign.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
