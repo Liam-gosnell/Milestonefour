bf814ff2f2c75bd510c9ebf3f4d95f6c6012b952
Liam-gosnell <liamgoz99@gmail.com>
2020-07-24 14:27:13 +0000

    Adding widgets to image field

--- orders/forms.py
+++ orders/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Item, ItemTag
 
 
@@ -7,6 +8,10 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = '__all__'
+
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
